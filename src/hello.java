139b72161b94e6a1bfaecb2b76885a32c4fba18f
竹小白 <1263020813@qq.com>
2019-08-13 22:44:52 +0800

    one time

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println(1+1);
+        System.out.println(3+3);
+
     }
 }
